7537d9b43c327f6f57efd746a0409212ea784239
Steve Longerbeam <steve_longerbeam@mentor.com>
2016-06-13 16:10:03 -0700

    gpu: ipu-v3: rename CSI client device
    Rename the CSI client device in the client_reg[] table to
    "imx-ipuv3-csi".
    
    Signed-off-by: Steve Longerbeam <steve_longerbeam@mentor.com>

--- drivers/gpu/ipu-v3/ipu-common.c
+++ drivers/gpu/ipu-v3/ipu-common.c
@@ -1153,14 +1153,14 @@ static struct ipu_platform_reg client_reg[] = {
 			.dma[0] = IPUV3_CHANNEL_CSI0,
 			.dma[1] = -EINVAL,
 		},
-		.name = "imx-ipuv3-camera",
+		.name = "imx-ipuv3-csi",
 	}, {
 		.pdata = {
 			.csi = 1,
 			.dma[0] = IPUV3_CHANNEL_CSI1,
 			.dma[1] = -EINVAL,
 		},
-		.name = "imx-ipuv3-camera",
+		.name = "imx-ipuv3-csi",
 	}, {
 		.pdata = {
 			.di = 0,
